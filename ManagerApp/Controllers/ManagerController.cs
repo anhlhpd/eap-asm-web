8a4fce5442cc147f849a7217fa99a9d88cd6af24
Tra <trauvmd00621@fpt.edu.vn>
2019-01-04 20:13:17 +0700

    init manager controller

---
+++ ManagerApp/Controllers/ManagerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagerApp.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
